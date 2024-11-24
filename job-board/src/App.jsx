24c054c27373c3e11ccba8167328211beec47cf1
Dylan Dalton <dylandalton.za@gmail.com>
2024-11-24 22:07:52 +0000

    Replaced navbar links with Navlinks for dynamic colouring

--- job-board/src/App.jsx
+++ job-board/src/App.jsx
@@ -8,6 +8,7 @@ import {
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
   const router = createBrowserRouter(
@@ -15,6 +16,7 @@ const App = () => {
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
     )
   );
